41f9d13a4630c415982a12f72dfae2409cb8715c
macklevine <mackplevine@gmail.com>
2015-07-13 21:26:38 -0700

    got some tests to pass and other delights

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,33 +4,17 @@
 var allMessagesOnServer;
 
 $(document).ready(function(){
-  setInterval(function(){getMessagesFromServer();}, 1000)
+  app.init();
 });
 
+var app = {};
 
-var Message = function(username, text, roomname){
-  //debugger;
-  this.image = null;
-  this.username = username;
-  this.text = text;
-  this.roomname = roomname;
-};
+app.init = function(){
+  setInterval(function(){app.fetch();}, 1000);
+}
+//
 
-var makeMessage = function(){
-  var userName = document.getElementById("username").value;
-  //debugger;
-  var currentMessage = document.getElementById("messagetext").value;
-  var roomname = document.getElementById("roomname").value;
-  var message = new Message(userName, currentMessage, roomname);
-  //debugger;
-  //$("body").append("<div>" + message.text + "</div>");
-  postMessagesToServer(message);
-  //return message;
-  //add functionality later to actually use a ajax post request with JSON.stringify(message)
-};
-
-
-var getMessagesFromServer = function(){
+app.fetch = function(){
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: 'https://api.parse.com/1/classes/chatterbox',
@@ -41,8 +25,7 @@ var getMessagesFromServer = function(){
     contentType: 'application/json',
     success: function (data) {
       allMessagesOnServer = data;
-      appendDataToFeed(allMessagesOnServer);
-      allMessagesOnServer;
+      app.appendDataToFeed(allMessagesOnServer);
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -51,15 +34,36 @@ var getMessagesFromServer = function(){
   });
 };
 
-var appendDataToFeed = function(allMessagesOnServer){
+app.appendDataToFeed = function(allMessagesOnServer){
   $(".messageFeed").text("");
-  for (var i = 0; i < allMessagesOnServer["results"].length; i++){
+  var theMessages = allMessagesOnServer["results"];
+  for (var i = 0; i < theMessages.length; i++){
     $(".messageFeed").append('<div class="postedMessage message' + i +'"></div>');
     $(".message" + i).text(allMessagesOnServer.results[i].username + ": " + allMessagesOnServer.results[i].text);
   }
+  var rooms = {}; //may want to make this an object
+  for (var j = 0; j < theMessages.length; j++){
+    if ("roomname" in theMessages[j]){
+      if (!theMessages[j].roomname in rooms){
+        rooms[theMessages[j].roomname] = theMessages[j].roomname;
+        //check to see if a given room is in the selection before we append it as a child to the node.
+      }
+    }
+  }
+  for (var key in rooms){
+    //if (!$('.rooms').children().each()) FIND A WAY TO ITERATE OVER THE CHILDREN OF .rooms TO TEST FOR PRESENCE OF KEY.
+    //if the key isn't there, let's append the key as a child element of the.rooms node.
+    $('.rooms').append('<option value="'+ key +'">'+ key +'</option>');
+  }
+  //console.log(rooms);
 };
 
-var postMessagesToServer = function(message){
+app.clearMessages = function(){
+  $("#chats").text("");
+  //debugger;
+}
+
+app.send = function(message){
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: 'https://api.parse.com/1/classes/chatterbox',
@@ -75,3 +79,24 @@ var postMessagesToServer = function(message){
     }
   });
 };
+
+app.makeMessage = function(){
+  var userName = document.getElementById("username").value;
+  //debugger;
+  var currentMessage = document.getElementById("messagetext").value;
+  var roomname = document.getElementById("roomname").value;
+  var message = new Message(userName, currentMessage, roomname);
+  //debugger;
+  //$("body").append("<div>" + message.text + "</div>");
+  app.send(message);
+  //return message;
+  //add functionality later to actually use a ajax post request with JSON.stringify(message)
+};
+
+var Message = function(username, text, roomname){
+  //debugger;
+  this.username = username;
+  this.text = text;
+  this.roomname = roomname;
+};
+
