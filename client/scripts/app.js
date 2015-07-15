f376753b1924ac8d036927cd2ea88fd4461ce198
macklevine <mackplevine@gmail.com>
2015-07-14 19:54:21 -0700

    fixed a bunch of stupid bullshit that we were pretty bad at initially

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,10 +1,12 @@
 // YOUR CODE HERE:
 
 var allMessagesOnServer;
-var selected;
+var selected = "Select a Room..."
+var friends = [];
 
 $(document).ready(function(){
   app.init();
+ //WE MAY HAVE TO MOVE THIS SOMEWHERE AND ADD THIS EVENT WATCHER EACH TIME OUR MESSAGE DIVS GET WIPED AND RE-APPENDED.
 });
 
 var app = {};
@@ -26,6 +28,7 @@ app.fetch = function(){
     success: function (data) {
       allMessagesOnServer = data;
       app.appendDataToFeed(allMessagesOnServer);
+      console.log("we're getting data");
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -39,20 +42,54 @@ var rooms = {};
 app.appendDataToFeed = function(allMessagesOnServer){
   $(".messageFeed").text("");
   var theMessages = allMessagesOnServer["results"];
-  for (var i = 0; i < theMessages.length; i++){
-    $(".messageFeed").append('<div class="postedMessage message' + i +'" data-room-name="' + theMessages[i].roomname + '"></div>');
-    $(".message" + i).text(theMessages[i].username + ": " + theMessages[i].text);
-    if($(".message" + i).data("room-name") === selected){
-      $(".message" + i).hide();
-    }
-  } //may want to make this an object
 
-  //ultimately, refactor the below out into their own functions called within the body of appendDataToFeed.
+  for (var i = 0; i < theMessages.length; i++){
+    if (app.validateData(theMessages[i])){
+      $(".messageFeed").append('<div class="postedMessage message' + i +'" data-room-name="' + theMessages[i].roomname + '"' + ' data-username="' + theMessages[i].username + '"></div>');
+      $(".message" + i).text(theMessages[i].username + ": " + theMessages[i].text);
+      if($(".message" + i).data("room-name") !== selected && selected !== "Select a Room..."){
+        $(".message" + i).hide();
+      }
+      if (friends.indexOf(theMessages[i].username) > -1){
+        $(".message" + i).css('font-weight', 'bold');
+      }
+      //app.boldFriend();
+    }
+  }
+  app.setHandlers();
+  app.populateRoomDropdown();
+};
+
+app.setHandlers = function(){
+  $(".postedMessage").click(function(){
+    var friended = $(this).data("username");
+    if (friends.indexOf(friended) === -1){
+      friends.push(friended);
+    }
+  });
+};
+
+app.validateData = function(message){
+  var OK = false;
+  _.each(message, function(key){
+    if (key && key.length > 0 || key === ""){
+      if (key.indexOf("<script>") === -1){
+        OK = true;
+      }
+    }
+  });
+  return OK;
+}
+
+app.populateRoomDropdown = function(){
+  var theMessages = allMessagesOnServer["results"];
+
   for (var j = 0; j < theMessages.length; j++){
     if ("roomname" in theMessages[j]){
       rooms[theMessages[j].roomname] = theMessages[j].roomname;
     }
   }
+
   for (var key in rooms){
     var found = false;
     for (j = 0; j < $(".rooms").children().length; j++){
@@ -65,9 +102,7 @@ app.appendDataToFeed = function(allMessagesOnServer){
       $('.rooms').append('<option value="'+ rooms[key] +'">'+ rooms[key] +'</option>');
     }
   };
-  //eventually, factor out the above...
-
-};
+}
 
 app.clearMessages = function(){
   $("#chats").text("");
@@ -80,7 +115,8 @@ app.filterMessages = function(){
   var menu = document.getElementsByClassName("rooms")[0];
   selected = menu.options[menu.selectedIndex].value; //does this get what we want?
   if (selected === "Select a room..."){
-    return; //test this...
+    $(".postedMessage").show();
+    return;
   }
   var messageDivsInFeed = $("#chats").children();
   _.each(messageDivsInFeed, function(div){
@@ -88,13 +124,6 @@ app.filterMessages = function(){
       $(div).hide();
     }
   });
-
-  // for (var i = 0; i < $(".postedMessage").length; i++){
-  //   if ($(".postedMessage")[i] !== selected){
-  //     $(".postedMessage")[i].hide();
-  //   }
-  // }
-
 }
 
 app.send = function(message){
